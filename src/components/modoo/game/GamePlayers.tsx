c01172d069824c4597fc06c7ed4c06ccdad31b71
Joo <joo@joostory.net>
2023-12-31 01:07:45 +0900

    winner 표시

--- src/components/modoo/game/GamePlayers.tsx
+++ src/components/modoo/game/GamePlayers.tsx
@@ -1,16 +1,31 @@
 import { ReceiveButton, SendButton } from "@/components/modoo/game/GameActionButton"
 import { ModooPlayer, currentGameState } from "@/state/modoo-state"
 import { toNumberFormat } from "@/utils/numberformat"
+import { SparklesIcon } from "@heroicons/react/24/solid"
+import { useMemo } from "react"
 import { useRecoilValue } from "recoil"
 
 function PlayerItem({player}: {player: ModooPlayer}) {
+  const currentGame = useRecoilValue(currentGameState)
+  const isWinner = useMemo(() => {
+    const sorted = [...currentGame.players].sort((a, b) => b.money - a.money)
+    const winner = sorted[0]
+    const looser = sorted[sorted.length - 1]
+    return player.money == winner.money && player.money > looser.money
+  }, [currentGame, player])
+
   return (
     <div className="flex min-w-0 gap-x-4 w-full">
       <div className="min-w-0 flex-auto grow">
-        <p className="text-xl font-semibold leading-6">
-          {player.name}
-        </p>
-        <p className="mt-2 truncate text-3xl leading-8">
+        <div className="flex w-full gap-x-2">
+          {isWinner &&
+            <SparklesIcon className="h-5 w-5 text-red-400" />
+          }
+          <p className="text-xl font-semibold leading-6">
+            {player.name}
+          </p>
+        </div>
+        <p className="mt-2 truncate text-3xl leading-8 text-yellow-100">
           {toNumberFormat(player.money)}
         </p>
       </div>
