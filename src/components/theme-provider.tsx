2914c887bad0278530c6fced3362657a03d81059
Joo <joo@joostory.net>
2024-11-09 21:26:07 +0900

    next-themes 오류 수정

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { ThemeProvider } from "next-themes"
-import { ThemeProviderProps } from "next-themes/dist/types"
+import { ThemeProviderProps } from "next-themes"
 
 export function BoardgameThemeProvider({children, ...props}: ThemeProviderProps) {
   return <ThemeProvider {...props}>{children}</ThemeProvider>
