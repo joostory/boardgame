d952ff31da8faa4a110e988d964a4d0f077c5a2b
Joo <joo@joostory.net>
2025-02-23 21:46:21 +0900

    grok이 만든 같은그림찾기, 사다리타기 게임 추가

---
+++ src/app/ladder/page.tsx
@@ -0,0 +1,10 @@
+import LadderApp from '@/components/ladder/LadderApp'
+import SubPageLayout from '@/components/layout/SubPageLayout'
+
+export default function Memory() {
+  return (
+    <SubPageLayout>
+      <LadderApp />
+    </SubPageLayout>
+  )
+}
