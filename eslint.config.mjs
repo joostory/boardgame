b452fbba201b29bc8ea0c3fc779ef2de5e255e44
Joo <joo@joostory.net>
2025-10-03 22:07:46 +0900

    eslint 적용

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from "eslint/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default defineConfig([{
+  files: ["src/**/*.tsx", "src/**/*.ts"],
+  extends: compat.extends("next/core-web-vitals"),
+
+  rules: {
+    "@next/next/no-img-element": "off",
+    "jsx-a11y/alt-text": "off",
+    "react-hooks/exhaustive-deps": "off",
+  },
+}]);
